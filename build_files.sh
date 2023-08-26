07021193b6889bff747550b3c851225485a77d92
pankil-soni <116267467+pankil-soni@users.noreply.github.com>
2023-08-26 23:25:57 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,9 @@
 # build_files.sh
 pip install -r requirements.txt
 
+pip install psycopg2-binary
+sqlite3 db.sqlite .dump > sqlite_dump.sql
+psql -U your_username -d your_database_name -f sqlite_dump.sql
 
 # make migrations
 python3.9 manage.py makemigrations
